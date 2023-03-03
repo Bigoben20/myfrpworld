d4d9e2e5b9f963c22af1668b3202c26f6f063afe
Kerem Cikar <k.bilge3817@gmail.com>
2023-03-03 12:59:13 +0300

    laravel valet

--- resources/views/livewire/includes/stories/story-create.blade.php
+++ resources/views/livewire/includes/stories/story-create.blade.php
@@ -16,7 +16,7 @@
     @enderror
     <form wire:submit.prevent="store">
 
-        <div class="flex flex-col border border-gray-800 rounded-xl">
+        <div class="flex flex-col border border-gray-800 rounded-xl" x-data="{name: '', genre:'', logline:''}">
             {{-- header --}}
             <div class="bg-gray-700 rounded-t-lg px-2 py-1 flex justify-start items-center">
                 <h2 class="text-slate-200 text-xl font-bold tracking-wide">Create Story</h2>
@@ -28,27 +28,32 @@
                 {{-- Name --}}
                 <div class="flex flex-col  dark:text-gray-200">
                     <label for="name">Name</label>
-                    <input type="text" wire:model.defer="storyName" id="name" placeholder="Story Name"
+                    <input type="text" wire:model.defer="storyName" x-model="name" placeholder="Story Name"
                         class="border border-gray-300 ddark:border-gray-500 rounded-lg focus:ring-sky-500 focus:ring-offset-0 dark:bg-gray-600 dark:text-gray-200">
                 </div>
 
                 {{-- Genre --}}
                 <div class="flex flex-col  dark:text-gray-200">
                     <label for="genre">Genre</label>
-                    <input type="text" wire:model.defer="storyGenre" id="genre" placeholder="Story Genre"
+                    <input type="text" wire:model.defer="storyGenre" x-model="genre" placeholder="Story Genre"
                         class="border border-gray-300 ddark:border-gray-500 rounded-lg focus:ring-sky-500 focus:ring-offset-0 dark:bg-gray-600 dark:text-gray-200">
                 </div>
 
                 {{-- Detail --}}
                 <div class="flex flex-col  dark:text-gray-200">
                     <label for="logline">Logline</label>
-                    <input type="text" wire:model.defer="storyLogline" id="logline" placeholder="Story logline" maxlength="255"
+                    <input type="text" wire:model.defer="storyLogline" x-model="logline" placeholder="Story logline" maxlength="255"
                         class="border border-gray-300 ddark:border-gray-500 rounded-lg focus:ring-sky-500 focus:ring-offset-0 dark:bg-gray-600">
                 </div>
             </div>
 
             <div class="p-2">
-                <x-generalButton color="green" class="px-12 w-full md:w-fit" type="submit">Save</x-generalButton>
+                <template x-if="name.length > 0 && genre.length > 0 && logline.length > 0">
+                    <x-generalButton color="green" class="w-full md:w-fit" type="submit">Save</x-generalButton>
+                </template>
+                <template x-if="name.length < 1 || genre.length < 1 || logline.length < 1">
+                    <button class="px-3 py-2.5 w-full md:w-fit bg-slate-500 opacity-50 text-white font-medium text-md leading-tight rounded-xl" type="button" disabled>Save</button>
+                </template>
             </div>
         </div>
     </form>
